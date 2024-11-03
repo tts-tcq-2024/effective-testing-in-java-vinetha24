320b0c00b685c3c2ed26778e4cb70f61e6b31eca
vinetha24 <113199878+vinetha24@users.noreply.github.com>
2024-11-03 17:39:06 +0530

    Update misaligned.java
    Using method references

--- misaligned/misaligned.java
+++ misaligned/misaligned.java
@@ -1,18 +1,64 @@
-public class Misaligned {
-    static int printColorMap() {
-        String majorColors[] = {"White", "Red", "Black", "Yellow", "Violet"};
-        String minorColors[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-        int i = 0, j = 0;
-        for(i = 0; i < 5; i++) {
-            for(j = 0; j < 5; j++) {
-                System.out.printf("%d | %s | %s\n", i * 5 + j, majorColors[i], minorColors[i]);
+public class Main {
+    static String actualManual = "";
+
+    public static void printOnConsole(String manualItem) {
+        System.out.print(manualItem);
+    }
+
+    public static int printColorMap(Printer printer) {
+        String[] majorColor = {"White", "Red", "Black", "Yellow", "Violet"};
+        String[] minorColor = {"Blue", "Orange", "Green", "Brown", "Slate"};
+        int count = 0;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                printer.print(i * 5 + j + " | " + majorColor[i] + " | " + minorColor[j] + "\n");
+                count++;
             }
         }
-        return i * j;
+        return count;
     }
-    public static void main(String[] args) { 
-        int result = printColorMap();
-        assert(result == 25);
+
+    public static void main(String[] args) {
+        int result = printColorMap(Main::mockPrintOnConsole);
+        assert result == 25;
+
+        String expectedConsoleBuffer =
+                "0 | White | Blue\n" +
+                "1 | White | Orange\n" +
+                "2 | White | Green\n" +
+                "3 | White | Brown\n" +
+                "4 | White | Slate\n" +
+                "5 | Red | Blue\n" +
+                "6 | Red | Orange\n" +
+                "7 | Red | Green\n" +
+                "8 | Red | Brown\n" +
+                "9 | Red | Slate\n" +
+                "10 | Black | Blue\n" +
+                "11 | Black | Orange\n" +
+                "12 | Black | Green\n" +
+                "13 | Black | Brown\n" +
+                "14 | Black | Slate\n" +
+                "15 | Yellow | Blue\n" +
+                "16 | Yellow | Orange\n" +
+                "17 | Yellow | Green\n" +
+                "18 | Yellow | Brown\n" +
+                "19 | Yellow | Slate\n" +
+                "20 | Violet | Blue\n" +
+                "21 | Violet | Orange\n" +
+                "22 | Violet | Green\n" +
+                "23 | Violet | Brown\n" +
+                "24 | Violet | Slate\n";
+
+        assert actualManual.equals(expectedConsoleBuffer);
+
         System.out.println("All is well (maybe!)");
+    }
+
+    public static void mockPrintOnConsole(String manualItem) {
+        actualManual += manualItem;
+    }
+
+    interface Printer {
+        void print(String manualItem);
     }
 }
