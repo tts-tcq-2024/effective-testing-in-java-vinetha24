aa1582e8a96df954e1b82709006b75d324d6b77d
vinetha24 <113199878+vinetha24@users.noreply.github.com>
2024-11-03 18:22:38 +0530

    Update NetworkAlertStub.java

--- alerter/NetworkAlertStub.java
+++ alerter/NetworkAlertStub.java
@@ -1,4 +1,5 @@
-class NetworkAlertStub implements NetworkAlertFunc {
+package alerter;
+public class NetworkAlertStub implements NetworkAlertFunc {
     public int alert(float celcius) {
         System.out.println("ALERT: Temperature is " + celcius + " celcius.");
         return 500;
